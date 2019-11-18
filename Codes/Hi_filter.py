b35159e287de2b68decf990eebbbe4f2d4d9e183
hyuck2 <hyuck2@outlook.kr>
2019-11-18 18:45:11 +0900

    1118

---
+++ Codes/Hi_filter.py
@@ -0,0 +1,16 @@
+def _sigma(data, start, end):
+    sum = 0
+    for i in range(start, end):
+        sum = sum + data[i]
+    return sum
+
+def _maf(data, parameter):
+    filtered = []
+    for i in range(len(data)):
+
+    return filtered
+
+def _lpf(data, parameter):
+    filtered = []
+    
+    return filtered
---
+++ Codes/PDRS.py
@@ -0,0 +1,3 @@
+from Hi_filter import *
+from Hi_map import * 
+from Hi_graph import * 
